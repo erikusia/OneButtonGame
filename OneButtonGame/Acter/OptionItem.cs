bbc3c86f3375a135c9230497941ee82fa58dea2e
tehsi307 <nerunerunerune402@gmail.com>
2019-10-18 13:32:18 +0900

    プレイヤー修正

--- OneButtonGame/Acter/OptionItem.cs
+++ OneButtonGame/Acter/OptionItem.cs
@@ -16,14 +16,12 @@ namespace OneButtonGame.Acter
         private IGameObjectMediator mediator;
         Vector2 speed;
         Range range;
-        int optionNumber;
         public OptionItem(Vector2 position, GameDevice gameDevice, IGameObjectMediator mediator)
             : base("block", position, 64, 64, gameDevice)
         {
             this.position = position;
             this.mediator = mediator;
             speed = new Vector2(0, 1);
-            optionNumber = 0;
         }
 
         public OptionItem(OptionItem other)
@@ -38,29 +36,17 @@ namespace OneButtonGame.Acter
 
         public override void Hit(GameObject gameObject)
         {
-            if (gameObject is Player)
+            if(gameObject  is Player)
             {
-                optionNumber = optionNumber+ 1;
-               
-                if (optionNumber <= 5)
-
-                {
-                    Console.WriteLine(optionNumber);
-                    GamePlay.gameObject.Add(new Option(Player.playerPosition, gameDevice, mediator, gameObjectManager));
-                    isDeadFlag = true;
-                }
-                else
-                {
-                    isDeadFlag = true;
-                }
-                
+                isDeadFlag = true;
             }
 
         }
 
         public override void Update(GameTime gameTime)
         {
-            
+
+
             position += speed;
         }
     }
